93227c373632fc6528eae827e89be5b5ddcf7cff
Jose Fernando <87836325+JoseFernandoh@users.noreply.github.com>
2022-11-12 22:29:39 +0000

    Criação do metodo Criado()

--- .ccls-cache/@home@runner@Carvores/main.c
+++ .ccls-cache/@home@runner@Carvores/main.c
@@ -17,9 +17,81 @@ NO* esvaziar(NO *raiz);
 int altura(NO *raiz);
 
 int main(void) {
+  
+  NO *raiz;
+	bool criada = false;
+  int n;
 
+  do{
+
+    printf("-----------------------------");
+
+    printf("-----------------------------");
+    
+
+    switch(n){
+
+      case 1:
+          raiz = criar(raiz,criada);
+          criada = true;
+        break;
+
+      case 2:
+         
+        
+        break;
+
+      case 3:
+        
+        break;
+
+      case 4:
+        
+        break;
+
+      case 5:
+        
+        break;
+
+      default:
+
+        break;
+
+      
+    }
+    
+  }while(n != 9);
+  
+  return 0;
+}
+
+NO* criar(NO *raiz, bool criada){
+	
+	if(!criada){
+		return NULL;
+	}
+	
+	printf("Raiz da arvore ja criada");
+	
+	return raiz;
+	
+}
+
+NO* inserir(NO *raiz){
   
 
+}
+
+void imprimir(){
+
+  
+}
+
+NO* esvaziar(NO *raiz){
+  
+}
+
+int altura(NO *raiz){
 
   return 0;
 }
