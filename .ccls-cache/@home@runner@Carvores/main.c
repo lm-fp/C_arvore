10059e508904e60a57797bfb083ff9289813dc33
Jose Fernando <87836325+JoseFernandoh@users.noreply.github.com>
2022-11-12 22:16:18 +0000

    Criação do projeto emC

--- .ccls-cache/@home@runner@Carvores/main.c
+++ .ccls-cache/@home@runner@Carvores/main.c
@@ -1,6 +1,25 @@
+#include <stdlib.h>
 #include <stdio.h>
+#include <stdbool.h>
+
+typedef struct NO{
+	int num;
+	int nivel;
+	struct NO *direita;
+	struct NO *esquerda;
+	
+} NO;
+
+NO* criar(NO *raiz, bool criada);
+NO* inserir(NO *raiz);
+void imprimir();
+NO* esvaziar(NO *raiz);
+int altura(NO *raiz);
 
 int main(void) {
-  printf("Hello World\n");
+
+  
+
+
   return 0;
 }
