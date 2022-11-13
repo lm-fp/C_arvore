be58a1cfd74b619d7a4f965ac3cd0f70dd7b892a
Leticia Maria <93682245+lm-fp@users.noreply.github.com>
2022-11-13 00:29:56 +0000

    Criando o metodo de imprimir()

--- main.c
+++ main.c
@@ -12,9 +12,12 @@ typedef struct NO{
 
 NO* criar(NO *raiz, bool criada);
 void inserir(NO *raiz, int numero);
-void imprimir();
+void imprimir(NO *raiz);
 NO* esvaziar(NO *raiz);
 int altura(NO *raiz);
+void pre_ordem(NO *raiz);
+void ordem(NO *raiz);
+void pos_ordem(NO *raiz);
 
 int main(void) {
   
@@ -27,6 +30,7 @@ int main(void) {
     printf("---------------------\n");
     printf("Criar:              1\n");
     printf("Inserir:            2\n");
+    printf("Imprimir:           3\n");
     printf("");
     printf("Sair                9\n");
     printf("---------------------\n");
@@ -41,14 +45,14 @@ int main(void) {
 
       case 2:
         {
-        int numero;
+          int numero;
           printf("Escreva seu numero: \n");
           scanf("%d", &numero );
-        inserir(raiz,numero);
+          inserir(raiz,numero);
         break;
           }
       case 3:
-        
+        imprimir(raiz);
         break;
 
       case 4:
@@ -85,7 +89,7 @@ NO* criar(NO *raiz, bool criada){
 }
 
  void inserir(NO *raiz , int numero){
-if (raiz ==NULL)
+if (raiz == NULL)
 {
   raiz=(NO *)malloc(sizeof(NO));
   raiz->esquerda=NULL;
@@ -100,9 +104,29 @@ if (raiz ==NULL)
     }
   }
 }
-void imprimir(){
-
-  
+void imprimir(NO *raiz){
+  int x;
+  printf("\n---------------------------------\n");
+  printf("1 - Para imprimir em Pré-Ordem\n");
+  printf("2 - Para imprimir em Ordem\n");
+  printf("3 - Para imprimir em Pós-Ordem\n");
+  printf("Digite qualquer outro numero para voltar\n");
+  printf("---------------------------------\n");
+  scanf("%d", &x);
+  switch(x){
+    case 1:
+      pre_ordem(raiz);
+    break;
+    case 2:
+      ordem(raiz);
+    break;
+    case 3:
+      pos_ordem(raiz);
+    break;
+    default:
+      printf("Voltando...");
+    break;
+    }
 }
 
 NO* esvaziar(NO *raiz){
@@ -113,4 +137,28 @@ NO* esvaziar(NO *raiz){
 int altura(NO *raiz){
 
   return 0;
+}
+
+void pre_ordem(NO *raiz){
+  if(raiz != NULL){
+    printf("%d", raiz->num);
+    pre_ordem(raiz->esquerda);
+    pre_ordem(raiz->direita);
+  }
+}
+
+void ordem(NO *raiz){
+  if(raiz != NULL){
+    ordem(raiz->esquerda);
+    printf("%d", raiz->num);
+    ordem(raiz->direita);
+  }
+}
+
+void pos_ordem(NO *raiz){
+  if(raiz != NULL){
+    pos_ordem(raiz->esquerda);
+    pos_ordem(raiz->direita);
+    printf("%d", raiz->num);
+  }
 }
